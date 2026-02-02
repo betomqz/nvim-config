ce58f25afce109a71c5b703a57e2c744e17095ad
Alberto Márquez <50601838+betomqz@users.noreply.github.com>
2026-02-02 14:03:36 -0600

    1 more plugins (#2)
    * add catppuccin theme
    
    * add blink
    
    * add keymap to open netrw
    
    * add todo-comments plugin
    
    * add mini plugin
    
    * add treesitter plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,13 @@
 require('lazy').setup {
+  { import = 'plugins.catppuccin' },
   { import = 'plugins.guess-indent' },
   { import = 'plugins.gitsigns' },
   { import = 'plugins.telescope' },
   { import = 'plugins.nvim-lspconfig' },
+  { import = 'plugins.nvim-treesitter' },
   { import = 'plugins.conform' },
+  { import = 'plugins.blink' },
   { import = 'plugins.which-key' },
+  { import = 'plugins.todo-comments' },
+  { import = 'plugins.mini' },
 }
